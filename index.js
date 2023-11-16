70f7e814b7a2e90eefa2693bc1bc31cb2ff77a79
Damari <damaricruz650@gmail.com>
2023-11-16 15:55:26 -0600

    mi primer commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function Reservar(onClick) {
+    var reservationform = document.getElementById("Reservar").value;
+    var reservation_list = document.getElementById("reservation_list");
+  
+    reservationList.innerHTML + "<" + reservationform + ">";
+  }
+  
